ae6989716fccc0e62cf8a1676ba7d65aadee7083
Ohzai Yaqub <seina@sunypoly.edu>
2025-12-15 23:30:17 -0500

    - positioned four elements on screen with scaling for different resolutions

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 import pygame
 from datetime import datetime
 from hijridate import Gregorian
-from config import FPS, WIDTH, HEIGHT, FULLSCREEN, FONT_PATH, NAME, ADDRESS, BLACK_COLOR, WHITE_COLOR, FOREST_GREEN_COLOR, HIJRI_MONTH_NAMES
+from config import FPS, WIDTH, HEIGHT, FULLSCREEN, FONT_PATH, NAME, ADDRESS, BLACK_COLOR, WHITE_COLOR, RED_COLOR, FOREST_GREEN_COLOR, HIJRI_MONTH_NAMES
 
 def main():
     
@@ -17,10 +17,19 @@ def main():
     
     clock = pygame.time.Clock()
     
-    mockup = pygame.image.load('assets/mockup.jpg') # Temporary: Load background mockup image
+    # Temporary: Load background mockup image and scale
+    mockup = pygame.image.load('assets/mockup.jpg')
+    mockup = pygame.transform.scale(mockup, (width, height))
     
-    font_size = 35
-    font = pygame.font.Font(FONT_PATH, font_size)
+    # Calculate scale factors
+    scale_x = width / WIDTH
+    scale_y = height / HEIGHT
+    
+    # Load fonts
+    time_font = pygame.font.Font(FONT_PATH, int(130 * scale_y)) # Current time and countdown
+    title_font = pygame.font.Font(FONT_PATH, int(85 * scale_y)) # Name label, announcement label, countdown label
+    table_font = pygame.font.Font(FONT_PATH, int(24 * scale_y)) # Prayer times labels and prayer times values
+    detail_font = pygame.font.Font(FONT_PATH, int(40 * scale_y)) # Date, Hijri date, address, and announcement details
     
     running = True
     show_background = False # Temporary: Toggle for background display
@@ -30,8 +39,8 @@ def main():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 running = False
                 
-        # Temporary: Toggle background with 'b' key            
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_b:
+        # Temporary: Toggle background with spacebar key            
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 show_background = not show_background
         if show_background:
             screen.blit(mockup, (0, 0))
@@ -41,37 +50,29 @@ def main():
         # Get the current date and time
         current_datetime = datetime.now()
         time_str = current_datetime.strftime("%I:%M:%S %p")
-        date_str = current_datetime.strftime("%B %d, %Y")
+        date_str = current_datetime.strftime("%d %B %Y")
         
         # Get the Hijri date
         hijri_date = Gregorian(current_datetime.year, current_datetime.month, current_datetime.day).to_hijri()
         hijri_month_name = HIJRI_MONTH_NAMES[hijri_date.month]
-        hijri_str = f"{hijri_month_name} {hijri_date.day}, {hijri_date.year}"
+        hijri_str = f"{hijri_date.day} {hijri_month_name} {hijri_date.year}"
         
-        # Render the masjid name at the vertical center
-        name_surface = font.render(NAME, True, BLACK_COLOR)
-        name_rect = name_surface.get_rect(center=(width // 2, height // 2))
+        dates = date_str + " · " + hijri_str # Combine Gregorian and Hijri dates
+        
+        name_surface = title_font.render(NAME, True, RED_COLOR)
+        address_surface = detail_font.render(ADDRESS, True, RED_COLOR)
+        time_surface = time_font.render(time_str, True, RED_COLOR)
+        date_surface = detail_font.render(dates, True, RED_COLOR)
+        
+        name_rect = name_surface.get_rect(center=(int(1217 * scale_x), int(96 * scale_y)))
+        address_rect = address_surface.get_rect(center=(int(1217 * scale_x), int(170 * scale_y)))
+        time_rect = time_surface.get_rect(center=(int(363 * scale_x), int(174 * scale_y)))
+        date_rect = date_surface.get_rect(center=(int(369 * scale_x), int(68 * scale_y)))
+        
         screen.blit(name_surface, name_rect)
-        
-        # Render the address below the masjid name
-        address_surface = font.render(ADDRESS, True, BLACK_COLOR)
-        address_rect = address_surface.get_rect(center=(width // 2, (height // 2) + font_size + 10))
         screen.blit(address_surface, address_rect)
-        
-        # Render the current time below the address
-        text_surface = font.render(time_str, True, BLACK_COLOR)
-        text_rect = text_surface.get_rect(center=(width // 2, (height // 2) + 2 * font_size + 20))
-        screen.blit(text_surface, text_rect)
-        
-        # Render the Gregorian date below the time
-        date_surface = font.render(date_str, True, BLACK_COLOR)
-        date_rect = date_surface.get_rect(center=(width // 2, (height // 2) + 3 * font_size + 30))
+        screen.blit(time_surface, time_rect)
         screen.blit(date_surface, date_rect)
-        
-        # Render the Hijri date below the Gregorian date
-        hijri_surface = font.render(hijri_str, True, BLACK_COLOR)
-        hijri_rect = hijri_surface.get_rect(center=(width // 2, (height // 2) + 4 * font_size + 40))
-        screen.blit(hijri_surface, hijri_rect)
         
         pygame.display.flip()
         clock.tick(FPS)
